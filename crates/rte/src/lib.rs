caeb0a97ca8e94bcf12b720ddcfc0274398e8d6f
Dolev Elbaz <dolevelbaz@microsoft.com>
2022-03-27 15:14:03 +0000

    Merged PR 5879590: Extract expirable to crate
    ### Description (Describe **what** was changed and don't skimp on the **how**)
    No functional changes, just separating expirable data structures code to another crate.
    
    ### Checklist
      - [ ] Code changes are covered by Unit Tests
      - [ ] Code changes are covered by Integration Tests
    
    Related work items: #13903815

--- crates/rte/src/lib.rs
+++ crates/rte/src/lib.rs
@@ -10,3 +10,13 @@ pub mod mempool;
 pub mod timer;
 
 type Result<T, E = rte_error::Error> = std::result::Result<T, E>;
+
+#[inline]
+pub fn get_tsc_hz() -> u64 {
+    unsafe { ffi::rte_get_tsc_hz() }
+}
+
+#[inline]
+pub fn get_tsc_cycles() -> u64 {
+    unsafe { ffi::_rte_get_tsc_cycles() }
+}
