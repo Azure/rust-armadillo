2fd4ef1d40a77aa11393fba243b36ebc8b18c5a7
Dolev Elbaz <dolevelbaz@microsoft.com>
2022-08-07 13:53:07 +0000

    Merged PR 6559094: Refactor crates/rte/launch module
    ### Description (Describe **what** was changed and don't skimp on the **how**)
    Remove similarity with the open-source RTE crate. For more details see [this thread](https://teams.microsoft.com/l/message/19:dbae25e8681842b094224fc2ee612a9c@thread.tacv2/1657189772489?tenantId=72f988bf-86f1-41af-91ab-2d7cd011db47&groupId=1a8efdfc-05ca-4cd8-bea0-a5b86817cb1a&parentMessageId=1657189772489&teamName=DDoS%20Engineering%20ILDC&channelName=General&createdTime=1657189772489).
    
    ### Checklist
      - [ ] Code changes are covered by Unit Tests
      - [ ] Code changes are covered by Integration Tests
    
    Related work items: #14960110

--- crates/rte/src/launch.rs
+++ crates/rte/src/launch.rs
@@ -1,12 +1,11 @@
-//! Launch tasks on other lcores
-//!
+//! Based on DPDK's `rte_launch.h` API: <https://doc.dpdk.org/api-21.08/rte__launch_8h.html>
+
 use std::os::raw::{c_int, c_void};
 
 use rte_error::ReturnValue as _;
 
 use crate::{lcore, Result};
 
-/// State of an lcore.
 #[repr(u32)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum State {
@@ -27,53 +26,47 @@ impl From<ffi::rte_lcore_state_t::Type> for State {
     }
 }
 
-// Definition of a remote launch function.
-pub type LcoreFunc<T> = fn(T) -> i32;
+type Entrypoint<T> = fn(T) -> i32;
 
-struct LcoreContext<T> {
-    callback: LcoreFunc<T>,
+struct ExecutionContext<T> {
+    entrypoint: Entrypoint<T>,
     arg: T,
 }
 
 unsafe extern "C" fn lcore_stub<T: Send + 'static>(arg: *mut c_void) -> c_int {
-    let LcoreContext { callback, arg } = *Box::from_raw(arg as *mut LcoreContext<T>);
-
-    callback(arg)
-}
-
-/// Launch a function on another lcore.
-///
-/// See docs for [`thread::spawn`](std::thread::spawn) for an explanation of the constraints on `T`.
-pub fn remote_launch_with_arg<T: Send + 'static>(callback: LcoreFunc<T>, arg: T, worker_id: lcore::Id) -> Result<()> {
-    let ctxt = Box::into_raw(Box::new(LcoreContext { callback, arg })) as *mut c_void;
-
-    unsafe { ffi::rte_eal_remote_launch(Some(lcore_stub::<T>), ctxt, worker_id.get()) }.rte_ok()?;
-    Ok(())
-}
-
-/// Launch a function on all lcores.
-///
-/// See docs for [`thread::spawn`](std::thread::spawn) for an explanation of the constraints on `T`.
-pub fn mp_remote_launch_with_arg<T: Send + 'static>(callback: LcoreFunc<T>, arg: T, skip_master: bool) -> Result<()> {
-    let ctxt = Box::into_raw(Box::new(LcoreContext { callback, arg })) as *mut c_void;
-    let call_main = if skip_master { ffi::rte_rmt_call_main_t::SKIP_MAIN } else { ffi::rte_rmt_call_main_t::CALL_MAIN };
-
-    unsafe { ffi::rte_eal_mp_remote_launch(Some(lcore_stub::<T>), ctxt, call_main) }.rte_ok()?;
-    Ok(())
+    let ExecutionContext { entrypoint, arg } = *Box::from_raw(arg as *mut ExecutionContext<T>);
+    entrypoint(arg)
 }
 
 impl lcore::Id {
-    /// Get the state of the lcore identified by lcore_id.
+    /// **NOTE:** should be executed on main lcore only. Will `panic` otherwise, if debug assertions are enabled.
+    ///
+    /// See docs for [`thread::spawn`](std::thread::spawn) for an explanation of the constraints on `T`.
+    #[inline]
+    pub fn launch<T: Send + 'static>(self, entrypoint: Entrypoint<T>, arg: T) -> Result<()> {
+        debug_assert!(lcore::current().is_main());
+        // Safety: memory is released in `lcore_stub` (success) or in the `Err` match arm (failure)
+        let ctxt = Box::into_raw(Box::new(ExecutionContext { entrypoint, arg })) as *mut c_void;
+        match unsafe { ffi::rte_eal_remote_launch(Some(lcore_stub::<T>), ctxt, self.get()) }.rte_ok() {
+            Ok(_) => Ok(()),
+            Err(err) => {
+                let _ = unsafe { Box::from_raw(ctxt) };
+                Err(err)
+            }
+        }
+    }
+
+    /// **NOTE:** should be executed on main lcore only. Will `panic` otherwise, if debug assertions are enabled.
+    #[inline]
     pub fn state(self) -> State {
+        debug_assert!(lcore::current().is_main());
         unsafe { ffi::rte_eal_get_lcore_state(self.get()) }.into()
     }
 }
 
-/// Wait until all lcores finish their jobs.
-///
-/// To be executed on the MASTER lcore only.
-/// Issue an rte_eal_wait_lcore() for every lcore.
-/// The return values are ignored.
-pub fn mp_wait_lcore() {
+/// **NOTE:** should be executed on main lcore only. Will `panic` otherwise, if debug assertions are enabled.
+#[inline]
+pub fn join_lcores() {
+    debug_assert!(lcore::current().is_main());
     unsafe { ffi::rte_eal_mp_wait_lcore() }
 }
